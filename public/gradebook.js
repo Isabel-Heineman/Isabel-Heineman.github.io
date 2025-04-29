8b1117b5b76ca4fbd41f47a01abe28c31e5cd581
Isabel-Heineman <ilheineman@my.waketech.edu>
2025-04-29 11:03:41 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -30,11 +30,11 @@ function populateGradebook(data) {
 	let tableElm = document.getElementById("gradebook"); // Get the gradebook table element
 		data.forEach((assignment) => { //For each row of data we're passed in
 			let row = document.createElement("tr"); //create a table row element
-			let columns = []; //Hnady place to stick the columns of information
+			let columns = []; // Handy place to stick the columns of information
 			columns.name = document.createElement('td'); //The first column's table data will be the name
 			columns.name.appendChild(
 				// Concatenate the full name: "last_name, first_name"
-				document.createTextNode(assignment.total_grade)
+				document.createTextNode("${assignment.last_name}, ${assignment.first_name}")
 			);
 			columns.grade = document.createElement('td'); // second column will be the grade
 			columns.grade.appendChild(
